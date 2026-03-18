9664310f0a9aa3f57081778e735648948e213811
Raja6284 <rajaraja360parth@gmail.com>
2026-03-18 15:01:47 +0530

    jagged and 3d array

--- Array.java
+++ Array.java
@@ -40,5 +40,29 @@ public class Array {
             }
             System.out.println();
         }
+
+        System.out.println();
+        int jagged[][] = new int[3][];
+
+        jagged[0] = new int[3];
+        jagged[1] = new int[4];
+        jagged[2] = new int[2];
+
+        for(int i = 0; i<3; i++){
+            for(int j = 0; j<jagged[i].length; j++){
+                jagged[i][j]= (int)(Math.random()*10);
+            }
+        }
+
+
+         for(int i = 0; i<3; i++){
+            for(int j = 0; j<jagged[i].length; j++){
+                
+                System.out.print(jagged[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
     }
 }
