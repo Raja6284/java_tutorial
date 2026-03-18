b25eb7a264d26c0aea7188d42100085c7797452b
Raja6284 <rajaraja360parth@gmail.com>
2026-03-18 14:35:33 +0530

    1d and 2d array

---
+++ Array.java
@@ -0,0 +1,35 @@
+
+
+public class Array {
+
+    public static void main(String args[]){
+
+        // int nums[] = {1,2,3,4,5};
+
+        // int nums[] = new int[5];
+
+        // for(int i= 0; i<nums.length; i++){
+        //     System.out.println(nums[i]);
+        // }
+
+        int nums[][] = new int[3][4];
+
+
+        for(int i = 0; i<3; i++){
+
+            for(int j = 0; j<4; j++){
+                nums[i][j] = (int)(Math.random()*10);
+            }
+           
+        }
+
+
+        for(int i = 0; i<3; i++){
+
+            for(int j = 0; j<4; j++){
+                System.out.print(nums[i][j]  + " ");
+            }
+            System.out.println();
+        }
+    }
+}
