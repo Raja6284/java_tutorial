ef65bf5d709b20567ed04af2a01a052e3f75d7e3
Raja6284 <rajaraja360parth@gmail.com>
2026-03-23 11:32:56 +0530

    simple inheritance

--- Demo.java
+++ Demo.java
@@ -1,35 +1,46 @@
 
 
-class A{
-    public A(){
-        System.out.println("Constructor of class A");
-    }
+// class A{
+//     public A(){
+//         System.out.println("Constructor of class A");
+//     }
 
-    public A(int x){
-        System.out.println("Parameterized constructor of class A");
-    }
+//     public A(int x){
+//         System.out.println("Parameterized constructor of class A");
+//     }
 
-}
+// }
 
-class B extends A{
-    public B(){
-        System.out.println("Constructor of class B");
-    }
+// class B extends A{
+//     public B(){
+//         System.out.println("Constructor of class B");
+//     }
 
-    public B(int x){
-        //super(x);
-        this();
-        System.out.println("Parameterized constructor of class B");
-    }
+//     public B(int x){
+//         //super(x);
+//         this();
+//         System.out.println("Parameterized constructor of class B");
+//     }
 
-}
+// }
+
+
+
+
+
 
 public class Demo{
-
     public static void main(String args[]){
-        //System.out.println("Hello World");
+        System.out.println("Hello World");
 
-        // A obj = new A();
-        B obj1 = new B(5);
+        //Calc obj = new Calc();
+        AdvCalc obj  = new AdvCalc();
+
+        int r1 = obj.add(3,4);
+        int r2 = obj.sub(5,2);
+        int r3 = obj.multi(3,4);
+        int r4 = obj.div(10,2);
+
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
     }
 }
