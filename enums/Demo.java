47cc5c944b85e7a97b9930f4d53fb6936ff38d5f
Raja6284 <rajaraja360parth@gmail.com>
2026-03-24 18:02:19 +0530

    Add Laptop enum and Demo class for laptop price management

---
+++ enums/Demo.java
@@ -0,0 +1,30 @@
+
+enum Laptop{
+    Loq(1222),dell(1344),mac,hp(11111);
+
+    private int price;
+
+    private Laptop(){
+        price = 200;
+    }
+
+    private Laptop(int price){
+        this.price = price;
+    }
+
+    public int getPrice(){
+        return this.price;
+    }
+
+}
+
+public class Demo {
+    public static void main(String args[]){
+
+        Laptop ll[] = Laptop.values();
+
+        for(Laptop l:ll){
+            System.out.println(l.ordinal() + " " + l + " " + l.getPrice());
+        }
+    }
+}
