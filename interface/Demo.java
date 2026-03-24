56f243c3bc8791bf3cc4966a718c02e438b9f26a
Raja6284 <rajaraja360parth@gmail.com>
2026-03-24 17:31:46 +0530

    Refactor Demo class and introduce interfaces A, Y, and Z for improved structure

--- interface/Demo.java
+++ interface/Demo.java
@@ -1,37 +1,76 @@
 
-abstract class Computer{
-    public abstract void code();
+// abstract class Computer{
+//     public abstract void code();
+// }
+
+// class Desktop extends Computer{
+//     public void code(){
+//         System.out.println("coding by desktop");
+//     }
+// }
+
+// class Laptop extends Computer{
+//     public void code(){
+//         System.out.println("Coding by laptop");
+//     }
+// }
+
+// class Developer{
+//     public void dev(Computer cmp){
+//         cmp.code();
+
+//     }
+// }
+
+// public class Demo{
+//     public static void main(String args[]){
+        
+//         //Laptop lap = new Laptop();
+
+//         Computer desk = new Desktop();
+//         Computer lap = new Laptop();
+
+//         Developer raja = new Developer();
+//         raja.dev(lap);
+//         raja.dev(desk);
+//     }
+// }
+
+
+interface A{
+    void show();
+    void config();
 }
 
-class Desktop extends Computer{
-    public void code(){
-        System.out.println("coding by desktop");
+interface Y{
+    void run();
+}
+
+interface Z extends Y{
+    //it will have all the method declaration of Y
+}
+
+class B implements A,Y{
+    public void show(){
+        System.out.println("in show");
+    }
+    public void config(){
+        System.out.println("in config");
+    }
+    public void run(){
+        System.out.println("in run");
     }
 }
 
-class Laptop extends Computer{
-    public void code(){
-        System.out.println("Coding by laptop");
-    }
-}
-
-class Developer{
-    public void dev(Computer cmp){
-        cmp.code();
-
-    }
-}
 
 public class Demo{
     public static void main(String args[]){
-        
-        //Laptop lap = new Laptop();
 
-        Computer desk = new Desktop();
-        Computer lap = new Laptop();
+        A obj ;
+        obj = new B();
 
-        Developer raja = new Developer();
-        raja.dev(lap);
-        raja.dev(desk);
+        obj.config();
+        obj.show();
+        //obj.run();
     }
 }
