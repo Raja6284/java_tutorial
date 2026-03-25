07fca5018ee02af92bf6e5bde49fc6e60a40f452
Raja6284 <rajaraja360parth@gmail.com>
2026-03-25 17:25:33 +0530

    Refactor input handling in Demo class to use Scanner for user input

--- Input/Demo.java
+++ Input/Demo.java
@@ -1,19 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Demo{
     public static void main(String args[]) throws IOException{
 
         System.out.println("Enter what you wnat: ");
 
-        InputStreamReader in = new InputStreamReader(System.in);
-        BufferedReader bf = new BufferedReader(in);
+        // InputStreamReader in = new InputStreamReader(System.in);
+        // BufferedReader bf = new BufferedReader(in);
 
-        int n = Integer.parseInt(bf.readLine());
-        System.out.println(n);
-        //System.out.println(bf.readLine());
+        // int n = Integer.parseInt(bf.readLine());
+        // System.out.println(n);
+        // //System.out.println(bf.readLine());
 
-        bf.close();
+        // bf.close();
+
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.nextLine();
+        System.out.println(num);
+
+        System.out.println("enter the string: ");
+
+       
+        String s = sc.nextLine();
+
+        System.out.println(s);
     }
 }
