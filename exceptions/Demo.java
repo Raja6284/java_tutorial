1487605f22456658cee34b7f7a618dc80ef26740
Raja6284 <rajaraja360parth@gmail.com>
2026-03-25 15:41:26 +0530

    Refactor Demo class to enhance exception handling and improve code clarity

--- exceptions/Demo.java
+++ exceptions/Demo.java
@@ -7,10 +7,23 @@ public class Demo{
 
         int ans = 0;
 
+        int nums[] = new int[5];
+        String str = null;
+
         try{
-            ans = j/i;
-        }catch(Exception e){
-            System.out.println("Something went wrong");
+            //ans = j/i;
+            System.out.println(nums[1]);
+            //System.out.println(nums[5]);
+            System.out.println(str.length());
+        }
+        catch(ArithmeticException e){
+            System.out.println("Cannot divide by zero " + e);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Stay in your limit " + e);
+        }
+        catch(Exception e){
+            System.out.println("Something went wrong " + e);
         }
 
         System.out.println(ans);
