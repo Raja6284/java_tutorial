ea94ea8359afe0339e3b34c44c152bb74ea44fb4
Raja6284 <rajaraja360parth@gmail.com>
2026-03-26 15:45:15 +0530

    Refactor Demo class to enhance random number generation and clean up commented code

--- CollectioInterface/Demo.java
+++ CollectioInterface/Demo.java
@@ -1,23 +1,43 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.*;
 
 public class Demo{
     public static void main(String args[]){
-        Collection<Integer> nums = new ArrayList<Integer>();
 
-        nums.add(3);
-        nums.add(33);
-        nums.add(3333);
-        nums.add(33333);
-        nums.add(333333);
+        //Collection<Integer> nums = new ArrayList<Integer>();
+        // List<Integer> nums = new ArrayList<Integer>();
 
-        System.out.println(nums.size());
+        // nums.add(3);
+        // nums.add(33);
+        // nums.add(3333);
+        // nums.add(33333);
+        // nums.add(333333);
+
+        // System.out.println(nums.size());
+
+        // for(int i = 0; i<nums.size(); i++){
+        //     System.out.println(nums.get(i));
+        // }
         
-        for(int n: nums){
-            System.out.println(n);
+        // for(int n: nums){
+        //     System.out.println(n);
+        // }
+
+        List<Integer> random = new ArrayList<Integer>();
+
+        for(int i = 0; i<100; i++){
+            random.add((int)(Math.random()*100));
         }
 
-        System.out.println(nums);
+        System.out.println(random.size());
+
+        // for(int i = 0; i<random.size(); i++){
+        //     System.out.println(random.get(i));
+        // }
+
+        System.out.println(random);
+       //System.out.println(nums);
         
     }
 }
