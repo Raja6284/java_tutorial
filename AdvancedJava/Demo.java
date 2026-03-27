a1c142ff065ae64b24b1a3a1ddf3dc8d25948249
Raja6284 <rajaraja360parth@gmail.com>
2026-03-27 13:54:10 +0530

    Consumer method for forEach

--- AdvancedJava/Demo.java
+++ AdvancedJava/Demo.java
@@ -1,26 +1,33 @@
-import java.util.Arrays;
 import java.util.*;
+import java.util.function.Consumer;
 
 public class Demo{
     public static void main(String args[]){
         System.out.println("hello");
 
         List<Integer> nums = Arrays.asList(2,3,4,5,6,7,8,9,10);
+        Consumer<Integer> con = new Consumer<Integer>(){
 
+            public void accept(Integer i){
+                System.out.println(i);
+            }
+        };
 
-        for(int i = 0; i<nums.size(); i++){
-            System.out.println(nums.get(i));
-        }
+        nums.forEach(con);
 
-        System.out.println();
+        // for(int i = 0; i<nums.size(); i++){
+        //     System.out.println(nums.get(i));
+        // }
 
-        for(int n: nums){
-            System.out.println(n);
-        }
+        // System.out.println();
 
-        System.out.println();
+        // for(int n: nums){
+        //     System.out.println(n);
+        // }
 
-        nums.forEach(n -> System.out.println(n));
+        // System.out.println();
+
+        // nums.forEach(n -> System.out.println(n));
 
     }
 }
