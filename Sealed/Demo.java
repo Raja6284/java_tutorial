5600d50ce442c6fd3f27f15ed29ad859ae3f4f6d
Raja6284 <rajaraja360parth@gmail.com>
2026-03-27 15:20:45 +0530

    sealed and non-sealed of interface

--- Sealed/Demo.java
+++ Sealed/Demo.java
@@ -15,6 +15,14 @@ final class D{
 
 }
 
+sealed interface X permits Y{
+
+}
+
+non-sealed interface Y extends X{
+    //interface can only be sealed and non-sealed, interface cannot be final
+}
+
 public class Demo{
     public static void main(String args[]){
         System.out.println("hello");
